ce07bb390b293cdc1a0322a2a369cc9825feaa15
nickupx <nick.upsky@gmail.com>
2020-12-12 18:38:43 -0800

    popover center

--- src/models/app.js
+++ src/models/app.js
@@ -2,39 +2,39 @@ import Popover from './popover'
 
 const button = document.getElementById('button')
 
-button.addEventListener('click', (e) => {
+button.addEventListener('click', () => {
   const popover = new Popover(button.dataset.title, button.dataset.body)
-  console.log(e.target.getBoundingClientRect())
   popover.render()
+  popover.element.style.top = '-' + ( popover.element.getBoundingClientRect().height + 8 ) + 'px'
   }
 )
 
-function showTooltip(e) {
-  e.preventDefault()
-  const query = document.querySelector('.tooltip')
-  const coords = this.getBoundingClientRect()
-  const hrefHeight = this.getBoundingClientRect().bottom - this.getBoundingClientRect().top
+// function showTooltip(e) {
+//   e.preventDefault()
+//   const query = document.querySelector('.tooltip')
+//   const coords = this.getBoundingClientRect()
+//   const hrefHeight = this.getBoundingClientRect().bottom - this.getBoundingClientRect().top
   
 
-  if (query) {
-      if (this.getAttribute('title') === query.textContent) {
-          query.remove()
-          return
-      }
-      query.remove()
-  }
-  let tooltip = document.createElement('div')
-  tooltip.classList.add('tooltip')
-  tooltip.style.display = 'block'
-  tooltip.style.top = coords.top + hrefHeight + 'px'
-  tooltip.style.left = coords.left + 'px'
-  tooltip.textContent = this.getAttribute('title')
-  document.body.append(tooltip)
+//   if (query) {
+//       if (this.getAttribute('title') === query.textContent) {
+//           query.remove()
+//           return
+//       }
+//       query.remove()
+//   }
+//   let tooltip = document.createElement('div')
+//   tooltip.classList.add('tooltip')
+//   tooltip.style.display = 'block'
+//   tooltip.style.top = coords.top + hrefHeight + 'px'
+//   tooltip.style.left = coords.left + 'px'
+//   tooltip.textContent = this.getAttribute('title')
+//   document.body.append(tooltip)
 
-  //setTimeout(() => tooltip.remove(), 2000)
-}
+//   //setTimeout(() => tooltip.remove(), 2000)
+// }
 
-let hrefs = document.getElementsByClassName('has-tooltip')
-for (href of hrefs) {
-  href.addEventListener('click', showTooltip)
-}
+// let hrefs = document.getElementsByClassName('has-tooltip')
+// for (href of hrefs) {
+//   href.addEventListener('click', showTooltip)
+// }
--- src/models/popover.js
+++ src/models/popover.js
@@ -3,19 +3,19 @@ export default class Popover {
     this.title = title
     this.body = body
     this.element = document.getElementById('popover')
+    this.container = document.getElementById('button-container')
   }
 
   render() {
     this.clear()
-    const container = document.getElementById('button-container')
     const element = document.createElement('div')
     element.id = 'popover'
     element.innerHTML = `
-      <div id="arrow" style="top: 34px;"></div>
       <h3 id="popover-title">${this.title}</h3>
       <div id="popover-body">${this.body}</div>
     `
-    document.body.appendChild(element)
+    this.container.appendChild(element)
+    this.element = element
   }
 
   clear() {
